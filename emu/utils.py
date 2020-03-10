7492a02af1af5499b39e1b6280ba0ee8e68ae91d
Elijah C <ejd.christensen@gmail.com>
2020-03-10 02:44:17 -0600

    behavioral data demos and utils code refactoring

--- emu/utils.py
+++ emu/utils.py
@@ -1,11 +1,20 @@
 import os
 import re
 import fnmatch
+import sys
 import pandas as pd
-import io
+import numpy as np
 from tqdm import tqdm as tqdm
 from boxsdk.object.file import File
 from boxsdk.object.folder import Folder
+from .luigi.box import ReadableBoxFile,BoxClient, DEFAULT_CONFIG_FP
+from .auth import Client
+if sys.version_info[0] < 3: 
+    from StringIO import StringIO
+else:
+    from io import StringIO
+
+DEFAULT_MANIFEST_FID = 588757437066
 
 _parse_ch = lambda fn: re.match('CSC(?P<channel>[0-9]+)(?P<block>_[0-9]{4})?\.ncs',fn)
 _get_ch = lambda f: int(_parse_ch(f).group('channel'))
@@ -13,29 +22,43 @@ _get_block = lambda f: _parse_ch(f).group('block')
 
 channel_fn_rgx = 'CSC(?P<channel>[0-9]+)(?P<block>_[0-9]{4})?\.ncs'
 
-def load_patients(client, file_id):
-    pt_manifest = client.file(file_id).get()
-    f = io.StringIO()
-    f.write(str(pt_manifest.content()))
-    f.seek(0)
-    return pd.read_csv(f)
-
-def get_file_manifest(folder, parent=None, prog_bar=False, **kwargs):
-    if parent is not None:
-        parent = os.path.join(parent,folder.name)
+def create_or_reuse_client(client):
+    if client is None:
+        return BoxClient()
     else:
-        parent = os.path.join('.')
+        return client
 
-    folder_info = folder.get()
-    total_count = folder_info.item_collection['total_count']
+def load_patients(box_client=None, path='/EMU/_pt_manifest.csv',file_id=None):
+    client = create_or_reuse_client(box_client)
+
+    if file_id is None:
+        file_id = DEFAULT_MANIFEST_FID
+    rbf = ReadableBoxFile(file_id,client)
+    # pt_manifest = client.file(file_id).get()
+    with StringIO(str(rbf.read(),'utf-8')) as infile:
+        df = pd.read_csv(infile,dtype={'filename':str,'type':str, 'id':np.int,'path':str})
+    return df
+
+def get_file_manifest(folder, prog_bar=False, **kwargs):
+    folder = folder.get()
+
+    # if parent is not None:
+    #     parent = os.path.join(parent,folder.name)
+    # else:
+    #     parent = os.path.join('.')
+
+    total_count = folder.item_collection['total_count']
+    folder_name = folder.name
     file_types={
         '.ncs':'Channel',
         '.nev':'Event',
         '.nde': 'Error',
+        '.csv': 'CSV',
+        '.mat': 'Matfile',
     }
-    for f in tqdm(folder.get_items(**kwargs),total=total_count):
+    for f in tqdm(folder.get_items(**kwargs),total=total_count,desc=folder_name):
         if isinstance(f,Folder):
-            get_file_manifest(f, parent=parent)
+            get_file_manifest(f)
         elif isinstance(f, File):
             if f.name[-4:] in file_types.keys():
                 ftype = file_types[f.name[-4:]]
@@ -44,19 +67,30 @@ def get_file_manifest(folder, parent=None, prog_bar=False, **kwargs):
             rec = {
                 'filename':f.name,
                 'id':f.id,
-                'path':os.path.join(parent,f.name),
+                # 'path':os.path.join(parent,f.name),
                 'type': ftype
             }
             yield rec
 
 class Experiment(object):
-    _pt_manifest_file_id = 588757437066
 
-    def __init__(self, client, pt_manifest):
-        self._client = client
-        self.client = client
-        self._patient_manifest = load_patients(client, self._pt_manifest_file_id)
-        self.patient_id = None
+    def __init__(self, study, patient_id, client=None, pt_manifest_file_id=None):
+        self._client = create_or_reuse_client(client)
+        if pt_manifest_file_id is None:
+            pt_manifest_file_id = DEFAULT_MANIFEST_FID
+
+        self._pt_manifest_file_id = pt_manifest_file_id
+
+        # self.client = client
+        self.patient_id = patient_id
+        self.study = study
+
+        _pm = load_patients(
+            self._client,
+            self._pt_manifest_file_id,
+            )
+        self._patient_manifest = _pm.query('study == "{}"'.format(self.study))
+        self._patient_manifest = self._patient_manifest.query('patient_id == {}'.format(self.patient_id))
         self.channels = None
 
     def __repr__(self):
@@ -66,14 +100,13 @@ class Experiment(object):
             exp_info = ''
         return '<EMU Experiment{}>'.format(exp_info)
 
-    def files(self, patient_id):
-        self.patient_id = patient_id
-        self.folder_id = self._patient_manifest.query('patient_id == {}'.format(patient_id)).iloc[0].folder_id
-        root = self.client.folder(self.folder_id)
-        self.folder_info = root.get()
-
-        records = get_file_manifest(root)
-        self.manifest = pd.DataFrame.from_records(records)
+    def files(self):
+        records = []
+        for fold in self._patient_manifest.folder_id:
+            root = self._client.folder(fold)
+            folder_files = get_file_manifest(root)
+            records.extend(folder_files)
+        return pd.DataFrame.from_records(records)
 
     def load_channels(self):
         if self.channels is None:
